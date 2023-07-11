193c7e91d9a3badc421d3bd7dfd4aeb8ad0c80c5
Adi <heydariiadi@gmail.com>
2023-07-10 17:29:23 -0700

    all file was changed

---
+++ tailwind.config.js
@@ -0,0 +1,55 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+   
+    colors: {  
+      Tint1: '#E5F2E9',
+      Tint2: '#CAE4D3',
+      Tint3: '#96C9A7',
+      Tint4: '#7CBC91',
+      Tint5: '#61AE7B',
+      Tint6: '#61AE7B',
+      Tint7: '#4E9968',
+      Primary: '#417F56',
+      Shade1: '#396F4B',
+      Shade2: '#315F41',
+      Shade5: '#183020',
+      Shade6: '#102016',
+      Shade7: '#08100B',
+      white: '#FFFFFF',
+      Gray1: '#F9F9F9',
+      Gray2: '#E1E1E1',
+      Gray3: '#EDEDED',
+      Gray4: '#CBCBCB',
+      Gray5: '#ADADAD',
+      Gray6: '#757575',
+      Gray7: '#717171',
+      Gray8: '#353535',
+      Black: '#0C0C0C',
+      Error: '#C30000',
+      Errorlight: '#ED2E2E',
+      Errorextralight: '#FFF2F2',
+      Success: '#00966D',
+      successlight: '#00BA88',
+      successextralight: '#F3FDFA',
+      warning: '#A9791C',
+      warninglight: '#F4B740',
+      warningextralight: '#FFF8E1',
+      
+    
+      
+    },
+    
+    extend: {
+      fontFamily:{
+        sans: ['Estedad'],
+      },
+
+    
+   
+    
+    },
+  },
+  plugins: [],
+}
